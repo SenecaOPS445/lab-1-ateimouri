a430dce188d312e5f233921457fe27b91abbbcea
Ali Teimouri <ateimouri1@myseneca.ca>
2024-05-10 10:05:12 -0400

    Added lab1c

---
+++ lab1c.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+'''
+    The script should have a Shebang line.
+    The script should have an object called name
+    The script should have an object called age
+    The value of the name object should be Isaac
+    The object age should contain a integer
+    The value of the age object should be 72
+    The script, when executed, should print out "Isaac is 72 years old!"
+    Example run:
+'''
+
+name='Isaac'
+age=72
+print(name +' is '+ str(age) + ' years old!')
