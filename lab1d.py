a07003dc29b5c86611bbe26b7d2de5e215b43775
Ali Teimouri <ateimouri1@myseneca.ca>
2024-05-10 10:21:00 -0400

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+'''
+    The script should have a Shebang line.
+    The object x should contain a integer with the value 10
+    The object y should contain a integer with the value 2
+    The object z should contain a integer with the value 5
+    The script, when executed, should print out "10 + 2 * 5 = 20" (the printout should change if the values in the objects change)
+'''
+#define the variebles
+x=10
+y=2
+z=5
+#print the output of the calculation of the number
+print(str(x) + " + " + str(y) + " * " + str(z) + " = " + str(x+y*z))
